4f12897d3e5dd181ec3b4d988c516e9ec12516ef
evoosa <eevvoossaa@gmail.com>
2019-05-17 19:18:09 +0300

    added an exmaple file that works

--- bw.py
+++ bw.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
 
-dir = "C:\\Users\\evoosa\\Desktop\\"
-img_name = "illumaor.png"
+dir = "C:\\temp"
+img_name = "resized.png"
 img = os.path.join(dir, img_name)
 # comes BEFORE resizing!!!
 
 image = Image.open(img)
-image = image.convert('L') # convert image to black and white
+image = image.convert('1') # convert image to black and white
 image.save(os.path.join(dir, "fitted.jpg"))
--- resize_img.py
+++ resize_img.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import os
 
-dir = "C:\\Users\\evoosa\\Desktop\\"
-img_name = "poop.jpg"
-#img_name = "illumaor.png"
-img = os.path.join(dir, img_name)
+dir = "C:\\temp"
+img_name = "fitted.jpg"
+
+output_img = "resized.png"
+
+input_img = os.path.join(dir, img_name)
 
 choice = input("save ratio? n/y: ")
 height = int(input("height: "))
@@ -12,16 +14,16 @@ width = int(input("width: "))
 
 if choice == "n":
     # don't keep ratio
-    image = Image.open(img)
+    image = Image.open(input_img)
     image = image.resize((width, height), Image.ANTIALIAS)
-    image.save(os.path.join(dir, "fitted.jpg"))
+    image.save(os.path.join(dir, output_img))
 
 elif choice == "y":
     # keep ratio. relying on the wanted height.
-    image = Image.open(img)
+    image = Image.open(input_img)
     wpercent = (width / float(image.size[0]))
     hsize = int((float(image.size[1]) * float(wpercent)))
     image = image.resize((width, hsize), Image.ANTIALIAS)
-    image.save(os.path.join(dir, "fitted.jpg"))
+    image.save(os.path.join(dir, output_img))
 
 
